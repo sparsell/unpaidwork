da8a284cd6cf11d051ab572ca3232bed2427332b
Sonja Parsell <hello@sparsell.com>
2020-07-01 07:02:56 -0400

    api call success!

--- lib/unpaidwork/country.rb
+++ lib/unpaidwork/country.rb
@@ -5,6 +5,7 @@ module UnpaidWork
         @@all = []
 
         def initialize
+            make_country(country_data)
             @name = name
             @unpaid_men = unpaid_men
             @unpaid_women = unpaid_women
@@ -13,31 +14,48 @@ module UnpaidWork
             @total_men = total_men
             @total_women = total_women
             save
-    end
-
-    def self.get_countries(country_names)
-        country_names = country_names.collect do |name| "#{name["name"]}"     
-	    end
-        country_names.each_with_index do |(key, value), index|
-	    puts "#{index +1}. #{key}"
-
         end
 
+        # def self.get_countries(country_names)
+        #     country_names = country_names.collect do |name| "#{name["name"]}"     
+	    #     end
+        #     country_names.each_with_index do |(key, value), index|
+	    #     puts "#{index +1}. #{key}"
+        #     end
+        # end     
 
         def self.make_country(country_data)
-            country_data.collect do |key, value|
-                your_country = puts value[0]
-                return your_country
-            end
+            country_specific = country_data.collect do |key, value| value[0] end
+                men_unpaid = country_specific[0]
+                women_unpaid = country_specific[1]
+                men_paid = country_specific[2]
+                women_paid = country_specific[3]
+                total_men = country_specific[4]
+                total_women = country_specific[5]
         end
 
-    def self.get_country_data(country_data) #create country based on index number? 
+        def self.get_country_data(country_data) 
+        end
 
+        # def self.display_country_data(id)
+        #     puts "OECD data for #{id}, in minutes per day:"
+        #     puts "Unpaid work performed by:"
+        #     puts "     Men: #{unpaid_men}"
+        #     puts "     Women:#{unpaid_women}"
+        #     puts ""
+        #     puts "Paid work performed by:"
+        #     puts "     Men: #{paid_men}"
+        #     puts "     Women:#{paid_women}"
+    
+    
+        #     puts "Total work performed by:"
+        #     puts "     Men: #{total_men}"
+        #     puts "     Women:#{total_women}"
+        # end
 
         def self.check_for_countries
             UnpaidWork::API.get_country(id)
         end   
-
 
         def self.all
             check_for_countries if @@all == []
@@ -47,20 +65,6 @@ module UnpaidWork
         def save
             @@all << self    
         end
-
-    def get_country_data(input) #iterate with input from user
-
-    end
-
-    def self.look_for_countries
-        UnpaidWork::API.get_countries
-    end   
-
-    def self.all
-        look_for_countries if @@all == []
-        @@all
-    end
-
 
     end
 end

f2f4a29a150a9cbf823a4ac9dabe4843caaac395
sparsell <hello@sparsell.com>
2020-07-10 06:00:35 -0400

    display country data method fix

--- lib/unpaidwork/cli.rb
+++ lib/unpaidwork/cli.rb
@@ -57,7 +57,7 @@ module UnpaidWork
         end
 
         def print_country_names
-            country_names = ["AUS".colorize(:yellow) + " - Austria", "AUT".colorize(:yellow) + " - Austria", "BEL".colorize(:yellow) + " - Belgium", "CAN".colorize(:yellow) + " - Canada", "CHL".colorize(:yellow) + " - Chile", "DNK".colorize(:yellow) + " - Denmark", "EST".colorize(:yellow) + " - Estonia", "FIN".colorize(:yellow) + " - Finland", "FRA".colorize(:yellow) + " - France", "DEU".colorize(:yellow) + " - Germany", "GRC".colorize(:yellow) + " - Greece", "HUN".colorize(:yellow) + " - Hungary", "ISL".colorize(:yellow) + " - Iceland", "IRL".colorize(:yellow) + " - Ireland", "ISR".colorize(:yellow) + " - Israel", "ITA".colorize(:yellow) + " - Italy", "JPN".colorize(:yellow) + " - Japan", "KOR".colorize(:yellow) + " - Korea", "LVA".colorize(:yellow) + " - Latvia", "LTU".colorize(:yellow) + " - Lithuania", "LUX".colorize(:yellow) + " - Luxembourg", "MEX".colorize(:yellow) + " - Mexico", "NLD".colorize(:yellow) + " - Netherlands", "NZL".colorize(:yellow) + " - New Zealand", "NOR".colorize(:yellow) + " - Norway", "POL".colorize(:yellow) + " - Poland", "PRT".colorize(:yellow) + " - Portugal", "SVK".colorize(:yellow) + " - Slovak Republic", "SVN".colorize(:yellow) + " - Slovenia", "ESP".colorize(:yellow) + " - Spain", "SWE".colorize(:yellow) + " - Sweden", "CHE".colorize(:yellow) + " - Switzerland", "TUR".colorize(:yellow) + " - Turkey", "GBR".colorize(:yellow) + " - United Kingdom", "USA".colorize(:yellow) + " - United States"]
+            country_names = ["AUS".colorize(:yellow) + " - Austria", "AUT".colorize(:yellow) + " - Austria", "BEL".colorize(:yellow) + " - Belgium", "CAN".colorize(:yellow) + " - Canada", "CHL".colorize(:yellow) + " - Chile", "DNK".colorize(:yellow) + " - Denmark", "EST".colorize(:yellow) + " - Estonia", "FIN".colorize(:yellow) + " - Finland", "FRA".colorize(:yellow) + " - France", "DEU".colorize(:yellow) + " - Germany", "GRC".colorize(:yellow) + " - Greece", "HUN".colorize(:yellow) + " - Hungary", "ISL".colorize(:yellow) + " - Iceland", "IRL".colorize(:yellow) + " - Ireland", "ISR".colorize(:yellow) + " - Israel", "ITA".colorize(:yellow) + " - Italy", "JPN".colorize(:yellow) + " - Japan", "KOR".colorize(:yellow) + " - Korea", "LVA".colorize(:yellow) + " - Latvia", "LTU".colorize(:yellow) + " - Lithuania", "LUX".colorize(:yellow) + " - Luxembourg", "MEX".colorize(:yellow) + " - Mexico", "NLD".colorize(:yellow) + " - Netherlands", "NZL".colorize(:yellow) + " - New Zealand", "NOR".colorize(:yellow) + " - Norway", "POL".colorize(:yellow) + " - Poland", "PRT".colorize(:yellow) + " - Portugal", "SVK".colorize(:yellow) + " - Slovak Republic", "SVN".colorize(:yellow) + " - Slovenia", "ESP".colorize(:yellow) + " - Spain", "SWE".colorize(:yellow) + " - Sweden", "TUR".colorize(:yellow) + " - Turkey", "GBR".colorize(:yellow) + " - United Kingdom", "USA".colorize(:yellow) + " - United States"]
             country_names.each { |name| puts name }
         end    
            
@@ -67,30 +67,29 @@ module UnpaidWork
             id = gets.strip.upcase
             UnpaidWork::API.get_country(id)
             display_country_data(id)
-            binding.pry
         end
 
         def display_country_data(id)
-            UnpaidWork::Country.all.select do |id| country.id == "id"
+            UnpaidWork::Country.all.select do |id| 
             puts "According to the OECD, in #{id}, time spent in paid and unpaid work by gender is as follows (represented in minutes per day,):"
             puts ""
             puts "Unpaid work performed by:".colorize(:green)
-            puts "     Men: #{country.unpaid_men}"
-            puts "     Women: #{@unpaid_women}"
+            puts "     Men: #{id.unpaid_men}"
+            puts "     Women: #{id.unpaid_women}"
             puts ""
             puts "Paid work performed by:".colorize(:green)
-            puts "     Men: #{@paid_men}"
-            puts "     Women:#{@paid_women}"
+            puts "     Men: #{id.paid_men}"
+            puts "     Women:#{id.paid_women}"
             puts ""
             puts "Total work performed by:".colorize(:green)
-            puts "     Men: #{@total_men}"
-            puts "     Women:#{@total_women}"
+            puts "     Men: #{id.total_men}"
+            puts "     Women:#{id.total_women}"
             puts ""
             end
         end
 
         def see_all
-            UnpaidWork::Country.all
+            puts Country.all
         end
 
         def learn_more
